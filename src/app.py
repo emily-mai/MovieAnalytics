824626cedafea61c66a154e03f638c7f04b582c3
Emily Mai <emilymai.1999@gmail.com>
2020-10-21 22:35:01 -0700

    implemented table display for data

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output, State
 
-app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
 app.title = 'Movie Analytics'
 app.config['suppress_callback_exceptions'] = True
 dataframe = pd.DataFrame()
@@ -23,13 +23,38 @@ def update_output(n_clicks):
     if n_clicks is not None:
         print("RECEIVED REQUEST FROM CLIENT")
         dataframe = utils.parse_csv("../data/keywords.csv", True)
-        return [dash_table.DataTable(
+        table = dash_table.DataTable(
             columns=[{"name": i, "id": i} for i in dataframe.columns],
             data=dataframe.to_dict('records'),
-            filter_action="native",
-            sort_action="native",
+            # filter_action="native",
+            # sort_action="native",
             css=[{'selector': '.row', 'rule': 'margin: 0'}],
-        )]
+            fixed_rows={'headers': True},
+            page_size=500,
+            style_data_conditional=[
+                {
+                    'if': {'row_index': 'odd'},
+                    'backgroundColor': 'rgb(236,240,241)'
+                }
+            ],
+            style_header={'backgroundColor': 'rgb(158,180,202)',
+                          'fontWeight': 'bold'},
+            style_cell={
+                'backgroundColor': 'rgb(191,200,201)',
+                'color': 'black',
+                'overflow': 'hidden',
+                'textOverflow': 'ellipsis',
+                'maxWidth': 0,
+            },
+            tooltip_data=[
+                {
+                    column: {'value': str(value), 'type': 'markdown'}
+                    for column, value in row.items()
+                } for row in dataframe.to_dict('rows')
+            ],
+            tooltip_duration=None
+        )
+        return html.Div(id='data_table', children=table, style={'height': 800, 'width': 1000})
 
 
 @app.callback(
@@ -71,7 +96,7 @@ app.layout = html.Div(children=[
                 Welcome! This is the homepage of our movie analytics webapp!
             '''),
             html.Hr(),
-            dbc.Button('Send Request to Server ', id='button1', color="info", className="mr-1"),
+            dbc.Button('Display Table ', id='button1', color="info", className="mr-1"),
             html.Div(id='output-container-button', children=[]),
             html.Hr(),
 
@@ -82,4 +107,6 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
+    # dataframe = utils.parse_csv("../data/keywords.csv", True)
+    # print(dataframe)
     app.run_server(debug=True)
