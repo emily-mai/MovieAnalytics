2ab27a473fb1f62e6332f6f6af396f8fe8794b80
Emily Mai <emilymai.1999@gmail.com>
2020-10-22 13:29:01 -0700

    Implemented basic search + updated GUI

--- src/app.py
+++ src/app.py
@@ -11,50 +11,102 @@ from dash.dependencies import Input, Output, State
 app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
 app.title = 'Movie Analytics'
 app.config['suppress_callback_exceptions'] = True
-dataframe = pd.DataFrame()
+dataframe = utils.parse_csv("../data/keywords.csv", True)
+
+
+def display_table(dataset):
+    table = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in dataset.columns],
+        data=dataset.to_dict('records'),
+        css=[{'selector': '.row', 'rule': 'margin: 0'}],
+        fixed_rows={'headers': True},
+        # page_action='custom',
+        page_size=500,
+        page_current=0,
+        # sort_action="native",
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(236,240,241)'
+            }
+        ],
+        style_header={'backgroundColor': 'rgb(158,180,202)',
+                      'fontWeight': 'bold'},
+        style_table={'overflowX': 'auto'},
+        style_cell={
+            'backgroundColor': 'rgb(191,200,201)',
+            'color': 'black',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+            # 'maxWidth': 0,
+            'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+        },
+        tooltip_data=[
+            {
+                column: {'value': str(value), 'type': 'markdown'}
+                for column, value in row.items()
+            } for row in dataset.to_dict('rows')
+        ],
+        tooltip_duration=None
+    )
+    return html.Div(id='data_table', children=table, style={'height': 800, 'width': 1000})
+
+
+operators = [['>='],
+             ['<='],
+             ['<'],
+             ['>'],
+             ['!='],
+             ['='],
+             ['contains '],
+             ['datestartswith ']]
+
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if v0 == value_part[-1] and v0 in ("'", '"', '`'):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+    return [None] * 3
 
 
 @app.callback(
-    Output('output-container-button', 'children'),
-    Input('button1', 'n_clicks'),
-)
-def update_output(n_clicks):
-    global dataframe
+    Output('output-container-button', "children"),
+    Input('button1', "n_clicks"),
+    State('search-bar', "value"))
+def update_table(n_clicks, value):
     if n_clicks is not None:
-        print("RECEIVED REQUEST FROM CLIENT")
-        dataframe = utils.parse_csv("../data/keywords.csv", True)
-        table = dash_table.DataTable(
-            columns=[{"name": i, "id": i} for i in dataframe.columns],
-            data=dataframe.to_dict('records'),
-            # filter_action="native",
-            # sort_action="native",
-            css=[{'selector': '.row', 'rule': 'margin: 0'}],
-            fixed_rows={'headers': True},
-            page_size=500,
-            style_data_conditional=[
-                {
-                    'if': {'row_index': 'odd'},
-                    'backgroundColor': 'rgb(236,240,241)'
-                }
-            ],
-            style_header={'backgroundColor': 'rgb(158,180,202)',
-                          'fontWeight': 'bold'},
-            style_cell={
-                'backgroundColor': 'rgb(191,200,201)',
-                'color': 'black',
-                'overflow': 'hidden',
-                'textOverflow': 'ellipsis',
-                'maxWidth': 0,
-            },
-            tooltip_data=[
-                {
-                    column: {'value': str(value), 'type': 'markdown'}
-                    for column, value in row.items()
-                } for row in dataframe.to_dict('rows')
-            ],
-            tooltip_duration=None
-        )
-        return html.Div(id='data_table', children=table, style={'height': 800, 'width': 1000})
+        print(value)
+        filtering_expressions = value.split(' && ')
+        dataset = dataframe
+        for filter_part in filtering_expressions:
+            col_name, operator, filter_value = split_filter_part(filter_part)
+            print(col_name, operator, filter_value)
+            if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+                # these operators match pandas series operator method names
+                dataset = dataset.loc[getattr(dataset[col_name], operator)(filter_value)]
+            elif operator == 'contains':
+                dataset = dataset.loc[dataset[col_name].str.contains(filter_value)]
+            elif operator == 'datestartswith':
+                # this is a simplification of the front-end filtering logic,
+                # only works with complete fields in standard format
+                dataset = dataset.loc[dataset[col_name].str.startswith(filter_value)]
+
+        return display_table(dataset)
 
 
 @app.callback(
@@ -96,7 +148,8 @@ app.layout = html.Div(children=[
                 Welcome! This is the homepage of our movie analytics webapp!
             '''),
             html.Hr(),
-            dbc.Button('Display Table ', id='button1', color="info", className="mr-1"),
+            dbc.Input(id="search-bar", placeholder="Column name, operator, value", type="text"),
+            dbc.Button('Search', id='button1', color="info", className="mr-1"),
             html.Div(id='output-container-button', children=[]),
             html.Hr(),
 
