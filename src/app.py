ec5360f351fab3edc4d65575f5854a0e7c6c1c1a
Emily Mai <emilymai.1999@gmail.com>
2020-10-26 03:20:19 -0700

    added documentation and refactored search

--- src/app.py
+++ src/app.py
@@ -53,37 +53,6 @@ def display_table(dataset):
     return html.Div(id='data_table', children=table, style={'height': 800, 'width': 1000})
 
 
-operators = [['>='],
-             ['<='],
-             ['<'],
-             ['>'],
-             ['!='],
-             ['='],
-             ['contains '],
-             ['datestartswith ']]
-
-
-def split_filter_part(filter_part):
-    for operator_type in operators:
-        for operator in operator_type:
-            if operator in filter_part:
-                name_part, value_part = filter_part.split(operator, 1)
-                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
-                value_part = value_part.strip()
-                v0 = value_part[0]
-                if v0 == value_part[-1] and v0 in ("'", '"', '`'):
-                    value = value_part[1: -1].replace('\\' + v0, v0)
-                else:
-                    try:
-                        value = float(value_part)
-                    except ValueError:
-                        value = value_part
-                # word operators need spaces after them in the filter string,
-                # but we don't want these later
-                return name, operator_type[0].strip(), value
-    return [None] * 3
-
-
 @app.callback(
     Output('output-container-button', "children"),
     Input('button1', "n_clicks"),
@@ -91,22 +60,8 @@ def split_filter_part(filter_part):
 def update_table(n_clicks, value):
     if n_clicks is not None:
         print(value)
-        filtering_expressions = value.split(' && ')
-        dataset = dataframe
-        for filter_part in filtering_expressions:
-            col_name, operator, filter_value = split_filter_part(filter_part)
-            print(col_name, operator, filter_value)
-            if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
-                # these operators match pandas series operator method names
-                dataset = dataset.loc[getattr(dataset[col_name], operator)(filter_value)]
-            elif operator == 'contains':
-                dataset = dataset.loc[dataset[col_name].str.contains(filter_value)]
-            elif operator == 'datestartswith':
-                # this is a simplification of the front-end filtering logic,
-                # only works with complete fields in standard format
-                dataset = dataset.loc[dataset[col_name].str.startswith(filter_value)]
-
-        return display_table(dataset)
+        result = utils.search(dataframe, query=value)
+        return display_table(result)
 
 
 @app.callback(
--- src/utils.py
+++ src/utils.py
@@ -33,3 +33,65 @@ def parse_csv(filepath, contains_header=False):
         dataframe = pd.DataFrame(data)
     print(dataframe)
     return dataframe
+
+
+# declare comparison operators
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+
+# function to process search inputs
+def split_filter_part(filter_part):
+    """
+    :param filter_part: string formatted as "column operator value"
+    :return: filter_part formatted as list of strings, if contains operator else return empty list
+    """
+    # only split if operator is in the search input
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                # clean up strings
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if v0 == value_part[-1] and v0 in ("'", '"', '`'):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+    return [None] * 3
+
+
+def search(dataframe, query):
+    """
+    :param dataframe: dataframe object to perform search on
+    :param query: query string to filter data
+    :return: dataframe of query results
+    """
+    # split multiple queries and process sequentially
+    filtering_expressions = query.split(' && ')
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+        print(col_name, operator, filter_value)
+        # these operators match pandas series operator method names
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            dataframe = dataframe.loc[getattr(dataframe[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dataframe = dataframe.loc[dataframe[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dataframe = dataframe.loc[dataframe[col_name].str.startswith(filter_value)]
+    return dataframe
